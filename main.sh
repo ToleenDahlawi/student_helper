cfbb07f9d5f4aaea2b7ee23523b837676c7fc9a2
Toleend <toleend@ub22.myguest.virtualbox.org>
2025-05-19 22:43:27 +0300

    This is the main code

---
+++ main.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Main menu for student helper project
+
+while true; do
+  echo "=============================="
+  echo "     STUDENT HELPER     "
+  echo "=============================="
+  echo "1. Calculate High School GPA"
+  echo "2. Show UQU Majors Based on GPA"
+  echo "3. Calculate University Cumulative GPA"
+  echo "4. Check University Honor Eligibility"
+  echo "5. Exit"
+  echo "------------------------------"
+  read -p "Choose an option: " choice
+
+  case $choice in
+    1)
+      GPA_calculator.sh
+      ;;
+    2)
+      available_majors.sh 
+      ;;
+    3)
+     cgpa.sh
+      ;;
+    4)
+      Check_honors.sh
+      ;;
+    5)
+      echo "Thank you for using our program"
+      exit 0
+      ;;
+    *)
+      echo "Invalid choice. Please enter a number from 1 to 5."
+      ;;
+  esac
+
+  echo ""
+done
