d5e1517d3fa0450941057d8b92aeacaf167cec6a
Laila-1522 <laila.sabbag15@gmail.com>
2025-05-20 23:41:00 +0300

    gapt and aat functions

--- GPA_calculator.sh
+++ GPA_calculator.sh
@@ -14,3 +14,19 @@ function calculate_weighted {
       echo "Invalid input. Please enter a positive number."
     fi
   done
+while true; do
+    read -p "General Aptitude Test Score: " qd
+    if [[ "$qd" =~ ^[0-9]+(\.[0-9]+)?$ ]] && (( $(echo "$qd >= 0" | bc -l) )); then
+      break
+    else
+      echo "Invalid input. Please enter a positive number."
+    fi
+  done
+while true; do
+    read -p "Academic Achievement Test Score: " hs
+    if [[ "$hs" =~ ^[0-9]+(\.[0-9]+)?$ ]] && (( $(echo "$hs >= 0" | bc -l) )); then
+      break
+    else
+      echo "Invalid input. Please enter a positive number."
+    fi
+  done
