067c1829443f2bb7c935655a6da22bf662c0cab2
Laila-1522 <laila.sabbag15@gmail.com>
2025-05-20 23:44:09 +0300

    adding GPA function and comments

--- GPA_calculator.sh
+++ GPA_calculator.sh
@@ -14,6 +14,7 @@ function calculate_weighted {
       echo "Invalid input. Please enter a positive number."
     fi
   done
+#score input must be [0-100]
 while true; do
     read -p "General Aptitude Test Score: " qd
     if [[ "$qd" =~ ^[0-9]+(\.[0-9]+)?$ ]] && (( $(echo "$qd >= 0" | bc -l) )); then
@@ -30,3 +31,7 @@ while true; do
       echo "Invalid input. Please enter a positive number."
     fi
   done
+#calculating GPA based on Saudi educational system
+total_weight=$(echo "scale=2; ($th * 0.3) + ($qd * 0.3) + ($hs * 0.4)" | bc)
+  echo "Your Weighted GPA: $total_weight"
+}
