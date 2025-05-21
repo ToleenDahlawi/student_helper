f9dbdcb95da33737f94e1ff275a05c742575868b
Toleen <toleen21d@gmail.com>
2025-05-21 10:35:51 +0300

    This is the summary function

--- cgpa.sh
+++ cgpa.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
- #summary function
+# Function to display a summary of user's input and results
+function summary {
+  echo ""
+  echo " --- Summary ---"
+  echo " Gender: $student_gender"
+  echo " College: $selected_college"
+  echo " Weighted GPA: $total_weight"
+  echo " Eligibility Results:"
+  for major in "${eligible_majors[@]}"; do
+    echo "- $major"
+  done
+}
